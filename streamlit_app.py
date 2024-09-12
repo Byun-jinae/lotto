2ed6c8d73fe18f89f68763595b3873ba8d8fe0fe
JinAe <57822582+Byun-jinae@users.noreply.github.com>
2024-09-12 20:39:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,19 @@ import random
 def generate_random_numbers(n=6, min_val=1, max_val=45):
     return random.sample(range(min_val, max_val + 1), n)
 
+def get_color(number):
+    if 1 <= number <= 10:
+        return 'yellow'
+    elif 11 <= number <= 20:
+        return 'blue'
+    elif 21 <= number <= 30:
+        return 'red'
+    elif 31 <= number <= 40:
+        return 'gray'
+    elif 41 <= number <= 45:
+        return 'green'
+    return 'black'  # 기본 색상
+
 def plot_numbers_with_colors(numbers):
     fig, ax = plt.subplots()
     ax.set_xlim(0, 10)
@@ -13,8 +26,8 @@ def plot_numbers_with_colors(numbers):
     ax.axis('off')
     ax.set_aspect('equal')  # 축의 비율을 같게 설정
 
-    colors = np.random.rand(6, 3)  # 랜덤 색상 생성
-    for i, (number, color) in enumerate(zip(numbers, colors)):
+    for i, number in enumerate(numbers):
+        color = get_color(number)
         circle = plt.Circle((i * 1.5 + 1, 2), 0.8, color=color, ec='black')
         ax.add_artist(circle)
         ax.text(i * 1.5 + 1, 2, str(number), fontsize=16, ha='center', va='center', color='black')
@@ -25,6 +38,6 @@ st.title('랜덤 로또 번호 생성기')
 
 if st.button('번호 생성하기'):
     numbers = generate_random_numbers()
-
+    
     fig = plot_numbers_with_colors(numbers)
     st.pyplot(fig)
